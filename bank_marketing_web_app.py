f8c5e0e9ca2788b729a95c46fa420e2eabc74b73
madhav28 <madhav2800@gmail.com>
2023-10-02 11:03:32 -0400

    Added correlation heatmap

--- bank_marketing_web_app.py
+++ bank_marketing_web_app.py
@@ -7,16 +7,30 @@ bank_marketing_df = pd.read_csv("https://raw.githubusercontent.com/madhav28/Bank
 
 st.title("Bank Marketing Dataset Analysis")
 
+st.markdown("### Histogram Plot Analysis")
+
+st.markdown("#### Here, y tells us the outcome of subscription")
+
 col1, col2 = st.columns([1, 3])
 
 with col1:
-    st.markdown("### Histogram Plot Analysis")
     column_options = bank_marketing_df.columns
     column_name = st.selectbox("Select Column", column_options)
 
 with col2:
     fig = sns.histplot(data=bank_marketing_df, x=column_name, hue='y').figure
     st.pyplot(fig)
+    plt.close()
+
+st.markdown("### Correlation Heatmap")
+
+col1, col2 = st.columns([1, 3])
+
+with col2:
+    fig = sns.heatmap(data=bank_marketing_df.corr(), cmap='coolwarm').figure
+    st.pyplot(fig)
+    plt.close()
+
 
 
 
