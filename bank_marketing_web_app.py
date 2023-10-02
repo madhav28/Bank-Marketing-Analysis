8f7de0c870b387b8c1ce18fdebabb1ee1f031f78
madhav28 <madhav2800@gmail.com>
2023-10-02 12:02:23 -0400

    Added feature description

--- bank_marketing_web_app.py
+++ bank_marketing_web_app.py
@@ -5,8 +5,23 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 bank_marketing_df = pd.read_csv("https://raw.githubusercontent.com/madhav28/Bank-Marketing-Analysis/main/bank-full.csv", sep=';')
+customer_description_df = pd.read_csv("column_description.txt", names=['Type', 'Description'])
 
 st.title("Bank Marketing Dataset Analysis")
+
+st.markdown("### Understanding Data: Feature Description")
+
+feature_description = {}
+
+column_name = bank_marketing_df.columns
+
+feature_description['Column Name'] = column_name
+feature_description['Type'] = customer_description_df['Type']
+feature_description['Description'] = customer_description_df['Description']
+
+feature_description_df = pd.DataFrame(feature_description)
+
+st.table(feature_description_df)
 
 st.markdown("### Histogram Plot Analysis")
 
