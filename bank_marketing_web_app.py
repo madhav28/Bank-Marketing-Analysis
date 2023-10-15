fdbd841650f4d5f949fee912e098bb874d8d0c61
madhav28 <madhav2800@gmail.com>
2023-10-15 15:59:00 -0400

    Added tabs to the app

--- bank_marketing_web_app.py
+++ bank_marketing_web_app.py
@@ -4,55 +4,63 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-bank_marketing_df = pd.read_csv("https://raw.githubusercontent.com/madhav28/Bank-Marketing-Analysis/main/bank-full.csv", sep=';')
-customer_description_df = pd.read_csv("column_description.txt", names=['Type', 'Description'])
-
 st.title("Bank Marketing Dataset Analysis")
 
-st.markdown("### Understanding Data: Feature Description")
+tab1, tab2, tab3 = st.tabs(
+    ['About Data', 'Data Visualisation', 'Predictive Modelling'])
 
-feature_description = {}
+bank_marketing_df = pd.read_csv(
+    "https://raw.githubusercontent.com/madhav28/Bank-Marketing-Analysis/main/bank-full.csv", sep=';')
 
-column_name = bank_marketing_df.columns
+with tab1:
 
-feature_description['Column Name'] = column_name
-feature_description['Type'] = customer_description_df['Type']
-feature_description['Description'] = customer_description_df['Description']
+    customer_description_df = pd.read_csv(
+        "column_description.txt", names=['Type', 'Description'])
 
-feature_description_df = pd.DataFrame(feature_description)
+    feature_description = {}
 
-idx = np.linspace(1, 17, 17)
-idx = idx.astype(int)
+    column_name = bank_marketing_df.columns
 
-feature_description_df.index = idx
+    feature_description['Column Name'] = column_name
+    feature_description['Type'] = customer_description_df['Type']
+    feature_description['Description'] = customer_description_df['Description']
 
-st.table(feature_description_df)
+    feature_description_df = pd.DataFrame(feature_description)
 
-st.markdown("### Histogram Plot Analysis")
+    idx = np.linspace(1, 17, 17)
+    idx = idx.astype(int)
 
-st.markdown("#### Here, y tells us the outcome of subscription")
+    feature_description_df.index = idx
 
-col1, col2 = st.columns([1, 3])
+    st.table(feature_description_df)
 
-with col1:
-    column_options = bank_marketing_df.columns
-    column_name = st.selectbox("Select Column", column_options)
+with tab2:
+    st.markdown("### Histogram Plot Analysis")
 
-with col2:
-    fig = sns.histplot(data=bank_marketing_df, x=column_name, hue='y').figure
-    st.pyplot(fig)
-    plt.close()
+    st.markdown("#### Here, y tells us the outcome of subscription")
 
-st.markdown("### Correlation Heatmap")
+    col1, col2 = st.columns([1, 3])
 
-col1, col2 = st.columns([1, 3])
+    with col1:
+        st.markdown("### Control Panel")
+        column_options = bank_marketing_df.columns
+        column_name = st.selectbox("Select Column", column_options)
 
-with col2:
-    fig = sns.heatmap(bank_marketing_df.corr(numeric_only=True), cmap='coolwarm').figure
-    st.pyplot(fig)
-    plt.close()
+    with col2:
+        fig = sns.histplot(data=bank_marketing_df,
+                           x=column_name, hue='y').figure
+        st.pyplot(fig)
+        plt.close()
 
+    st.markdown("### Correlation Heatmap")
 
+    col1, col2 = st.columns([1, 3])
 
+    with col2:
+        fig = sns.heatmap(bank_marketing_df.corr(
+            numeric_only=True), cmap='coolwarm').figure
+        st.pyplot(fig)
+        plt.close()
 
-
+with tab3:
+    st.text("Work in progress...")
