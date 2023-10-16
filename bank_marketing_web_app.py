0b230587a1c4e2f1f091125628390fb526f5140e
madhav28 <madhav2800@gmail.com>
2023-10-15 22:02:54 -0400

    Added visualisation for Marketing Strategy tab

--- bank_marketing_web_app.py
+++ bank_marketing_web_app.py
@@ -65,7 +65,7 @@ with tab2:
                               'default', 'balance', 'housing', 'loan']
             column_name = st.selectbox("Column Name", column_options)
             bins = st.selectbox("Bins", np.linspace(
-                1, 10, 10).astype(int), 9, key='target_customers')
+                1, 10, 10).astype(int), 9, key='target_customers_hist')
 
         with col2:
             fig = sns.histplot(data=bank_marketing_df,
@@ -82,8 +82,10 @@ with tab2:
             column_options = ['age', 'marital', 'education', 'job',
                               'default', 'balance', 'housing', 'loan']
 
-            column_x = st.selectbox("Column for x-axis", column_options, 0)
-            column_y = st.selectbox("Column for y-axis", column_options, 5)
+            column_x = st.selectbox(
+                "Column for x-axis", column_options, 0, key='target_customers_sx')
+            column_y = st.selectbox(
+                "Column for y-axis", column_options, 5, key='target_customers_sy')
 
         with col2:
 
@@ -100,12 +102,12 @@ with tab2:
             "##### Hiplot Visualisation")
 
         credit_analysis_df = target_customers_df[[
-            'y', 'marital', 'balance', 'job', 'education', 'age']]
+            'y', 'marital', 'loan', 'balance', 'job', 'education', 'age']]
 
         exp = hip.Experiment.from_dataframe(credit_analysis_df)
 
         def save_hiplot_to_html(exp):
-            output_file = "hiplot_plot.html"
+            output_file = "hiplot_plot_1.html"
             exp.to_html(output_file)
             return output_file
 
@@ -119,8 +121,8 @@ with tab2:
 
         st.markdown("##### Histogram Plot Analysis")
 
-        strategy_df = bank_marketing_df[['contact', 'day', 'month', 'duration',
-                                         'campaign', 'pdays', 'previous', 'poutcome', 'y']]
+        strategies_df = bank_marketing_df[['contact', 'day', 'month', 'duration',
+                                           'campaign', 'pdays', 'previous', 'poutcome', 'y']]
 
         col1, col2 = st.columns([1, 3])
 
@@ -129,13 +131,55 @@ with tab2:
                               'campaign', 'pdays', 'previous', 'poutcome', 'y']
             column_name = st.selectbox("Column Name", column_options)
             bins = st.selectbox("Bins", np.linspace(
-                1, 10, 10).astype(int), 9, key='strategy')
+                1, 10, 10).astype(int), 9, key='strategies')
 
         with col2:
             fig = sns.histplot(data=bank_marketing_df,
                                x=column_name, hue='y', bins=bins).figure
             st.pyplot(fig)
             plt.close()
+
+        st.markdown("##### Scatter Plot Analysis")
+
+        col1, col2 = st.columns([1, 3])
+
+        with col1:
+
+            column_options = ['contact', 'day', 'month', 'duration',
+                              'campaign', 'pdays', 'previous', 'poutcome', 'y']
+
+            column_x = st.selectbox(
+                "Column for x-axis", column_options, 6, key='strategies_sx')
+            column_y = st.selectbox(
+                "Column for y-axis", column_options, 7, key='strategies_sy')
+
+        with col2:
+
+            custom_color_scale = alt.Scale(
+                domain=['no', 'yes'], range=['red', 'green'])
+
+            chart = alt.Chart(strategies_df).mark_circle().encode(
+                x=column_x, y=column_y, color=alt.Color('y:N',
+                                                        scale=custom_color_scale)).interactive()
+
+            st.altair_chart(chart, theme="streamlit", use_container_width=True)
+
+        st.markdown(
+            "##### Hiplot Visualisation")
+
+        strategy_analysis_df = strategies_df[[
+            'y', 'campaign', 'poutcome', 'previous']]
+
+        exp = hip.Experiment.from_dataframe(strategy_analysis_df)
+
+        def save_hiplot_to_html(exp):
+            output_file = "hiplot_plot_2.html"
+            exp.to_html(output_file)
+            return output_file
+
+        hiplot_html_file = save_hiplot_to_html(exp)
+        st.components.v1.html(
+            open(hiplot_html_file, 'r').read(), height=1500, scrolling=True)
 
 
 with tab3:
